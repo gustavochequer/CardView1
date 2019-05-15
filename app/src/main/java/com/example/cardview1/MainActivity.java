932ff772fbfaf4ff88b9611006f6ec9dccec3658
gustavo.coliveira15 <gustavochequer@gmail.com>
2019-05-15 11:55:06 -0300

    alteração dia 15/05

--- app/src/main/java/com/example/cardview1/MainActivity.java
+++ app/src/main/java/com/example/cardview1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.cardview1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater =  getMenuInflater();
+        inflater.inflate(R.menu.menu_cardview, menu);
+
+        return true;
+
+    }
 }
+
